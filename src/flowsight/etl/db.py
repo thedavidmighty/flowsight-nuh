fd087cf927016d443e9b0525c3997495bb96cdc8
thedavidmighty <thedavidukwu@gmail.com>
2025-05-10 15:25:29 +0100

    add database helper using sqlalchemy engine

---
+++ src/flowsight/etl/db.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import os
+PG_URI = os.getenv("PG_URI",
+                   "postgresql+psycopg2://flowsight:flowsight@loaclhost:5432/nuh_staging")
+
+ENGINE = create_engine(PG_URI, future=True)
+
+def write_to_db(df, table_name):
+    """
+    write a dataframe to a database table using sqlalchemy
+    """
+    df.to_sql(table_name, ENGINE, if_exists="replace", index=False, method="multi")
+
+def read_from_db(sql, params = None):
+    """
+    read a table from the datebase using sqlalchemy"""
+    return pd.read_sql(sql, ENGINE, params= params or {})
